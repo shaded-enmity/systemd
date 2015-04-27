77c977842d888b4af5ed1b135434040b3c7807ef
Pavel Odvody <podvody@redhat.com>
2015-04-27 11:06:02 +0200

    bearer token

--- src/import/pull-dkr.c
+++ src/import/pull-dkr.c
@@ -687,12 +687,10 @@ static void dkr_pull_job_on_finished_v2(PullJob *j) {
                 json_variant *e = NULL;
 
                 assert(!i->layer_job);
-                printf("===========================================================================================\n");
-                printf("JSON(%d bytes / %d):\n%s", j->payload_size, strlen(j->payload), (const char*)j->payload);
-                printf("===========================================================================================\n");/*
-                for (unsigned x = 0; x < j->payload_size; x++) {
-                        putchar(j->payload[x]);
-                }*/
+                log_info("===========================================================================================");
+                log_info("JSON(%"PRIu64" bytes):\n%s", j->payload_size, (const char*)j->payload);
+                log_info("===========================================================================================");
+
                 if (0 > json_parse((const char *)j->payload, &doc)) {
                         r = -EBADMSG;
                         log_error("Invalid JSON Manifest");
