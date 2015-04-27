8ebd0b8aff9aef40305fa0eaa4f7084690732c35
Pavel Odvody <podvody@redhat.com>
2015-04-27 15:21:28 +0200

    bearer token

--- src/import/pull-dkr.c
+++ src/import/pull-dkr.c
@@ -429,6 +429,8 @@ static int dkr_pull_add_bearer_token(DkrPull *i, PullJob *j) {
 
         if (i->response_token) {
                 t = strjoina("Authorization: Bearer ", i->response_token);
+        } else {
+                t = "NO TOKEN!"
         }
 
         log_info("token: %s", i->response_token);
